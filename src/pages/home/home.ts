a9226919fba965e4499eb18f5a6309850d62591f
Daniel <wuhaoxcp@gmail.com>
2017-11-02 00:19:22 +0800

    完成首页基本布局和tabs组件创建

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,12 +9,16 @@ import { MyHttpService } from '../../app/utility/service/myhttp.service';
 export class HomePage {
   carouselPic:Array<any>=[];
   nav:Array<any>=[];
+  productList:Array<any>=[];
+  loadNum=4;
+  loadTip="正在努力的加载中...";
   constructor(public myhttpCtrl:MyHttpService, public navCtrl: NavController) {
   
   }
   ionViewWillEnter(){
    this.getCarousel();
    this.getNav();
+   this.getProducts();
   }
   getCarousel(){  
     this.myhttpCtrl.sendRequest("../../assets/json/index_carousel.json")
@@ -29,5 +33,24 @@ export class HomePage {
       console.log(data);
       this.nav=data;
     })
+  }
+  getProducts(){
+    this.myhttpCtrl.sendRequest("../../assets/json/products.json")
+    .subscribe((data:any)=>{
+      this.productList=data;
+    })
+  }
+  loadMore(infiniteScroll){
+    if(this.loadNum<this.productList.length){
+      this.loadNum+=2;
+      this.getProducts();
+    }
+    else{
+      this.loadTip="没有新内容了";
+    }
+    setTimeout(()=>{
+      infiniteScroll.complete();
+    },1000)
+      
   }
 }

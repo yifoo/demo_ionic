004d321c11317ec447f60c9f0f44cb705a52a419
Daniel <wuhaoxcp@gmail.com>
2017-11-11 23:40:29 +0800

    delete

--- 24季11-6/src/assets/data/24ji.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS `ji_products`;
-CREATE TABLE ji_products(
-	pid				INT PRIMARY KEY AUTO_INCREMENT,
-	title			VARCHAR(36),
-	subtitle	VARCHAR(36),
-	aid				INT,
-);
--- 24季11-6/src/assets/data/product_list.php
+++
@@ -1,6 +0,0 @@
-<?php
-header('Content-Type: application/json;charset=UTF-8');
-require_once("init.php");
-$sql = "SELECT pi.pid,p.title,p.price,pi.img FROM ji_product p,ji_product_img pi GROUP by p.pid";
-echo json_encode(sql_execute($sql));
-?>
